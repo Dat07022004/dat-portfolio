e960014f574023910c7f8406be8c881adf782ebc
datdeptrai2k4 <128396435+datdeptrai2k4@users.noreply.github.com>
2025-10-10 19:55:25 +0700

    run build

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
 import { techStackIcons } from '../constants'
-import TechIcon from '../components/Models/TechLogos/techIcon'
+import TechIcon from '../components/Models/TechLogos/TechIcon'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 
